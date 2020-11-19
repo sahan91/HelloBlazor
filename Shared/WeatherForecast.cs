8e29e94ea1628f2e9d68f78ae8fc22b8527387c5
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2020-11-19 16:27:23 +1030

    Moved DTOs to a shared lib

--- Shared/WeatherForecast.cs
+++ Shared/WeatherForecast.cs
@@ -1,6 +1,8 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Server.Data
+namespace Shared
 {
     public class WeatherForecast
     {
@@ -8,8 +10,8 @@ namespace Server.Data
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
--- WASM/Pages/FetchData.cs
+++ WASM/Pages/FetchData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Shared;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
@@ -13,17 +11,6 @@ namespace WASM.Pages
         protected override async Task OnInitializedAsync()
         {
             forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
-        }
-
-        public class WeatherForecast
-        {
-            public DateTime Date { get; set; }
-
-            public int TemperatureC { get; set; }
-
-            public string Summary { get; set; }
-
-            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         }
     }
 }
